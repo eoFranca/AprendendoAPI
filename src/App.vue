12bcdfa9f9bf001c6063154201d94a7b3c522bdb
eoFranca <erfranca.com@gmail.com>
2023-10-24 11:55:50 -0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <header>
     <nav>
-      <router-link to="/">Home</router-link>
-      <router-link to="/filmes">Filmes</router-link>
-      <router-link to="/tv">Programas de TV</router-link>
+  <h1>IFWachers</h1>
+      <router-link to="/" class="navbar-link">Home</router-link>
+      <router-link to="/filmes" class="navbar-link">Filmes</router-link>
+      <router-link to="/tv" class="navbar-link">Programas de TV</router-link>
     </nav>
   </header>
   <main>
@@ -15,7 +16,7 @@
 header {
   height: 3rem;
   display: flex;
-  background-color: black;
+  background-color: #333; 
   color: #fff;
   font-size: 1.2rem;
   padding-left: 2rem;
@@ -28,8 +29,14 @@ nav {
   align-items: center;
 }
 
-nav a {
+.navbar-link {
   text-decoration: none;
   color: #fff;
+  margin: 0 1rem; 
+  transition: color 0.3s; 
+}
+
+.navbar-link:hover {
+  color: #0da34b; 
 }
 </style>
