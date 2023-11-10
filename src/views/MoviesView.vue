c2d159a3d69b03538e5908d4b9d9778876d415d3
eoFranca <erfranca.com@gmail.com>
2023-11-10 16:55:07 -0300

    enfatizando genero

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -2,75 +2,74 @@
 import { ref, onMounted } from 'vue'
 import api from '@/plugins/axios'
 import Loading from 'vue-loading-overlay'
-import genreStore from '@/stores/genres'
+import genreStore from '@/stores/genre'
 
 
 const isLoading = ref(false);
 
-const genres = ref([]);
 const movies = ref([]);
 
 const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR')
 
 
- function getGenreName(id) {
-    const genero = genres.value.find((genre) => genre.id === id);
-    return genero.name;
-  }
-  
-  onMounted(async () => {
+onMounted(async () => {
   isLoading.value = true
   await genreStore.getAllGenres('movie')
   isLoading.value = false
 })
 
 const listMovies = async (genreId) => {
+  genreStore.setCurrentGenreId(genreId);
   isLoading.value = true;
   const response = await api.get('discover/movie', {
     params: {
       with_genres: genreId,
-      language: 'pt-BR'
-    }
+      language: 'pt-BR',
+    },
   });
-  movies.value = response.data.results
+  movies.value = response.data.results;
   isLoading.value = false;
-};
+};  
+
 </script>
 <template>
   <div class="container">
 
     <h1>Filmes</h1>
     <ul class="genre-list">
-      <li
-      v-for="genre in genreStore.genres"
-      :key="genre.id"
-      @click="listMovies(genre.id)"
-      class="genre-item" >{{ genre.name }}</li></ul>
+      <li 
+        v-for="genre in genreStore.genres" 
+        :key="genre.id" 
+        @click="listMovies(genre.id)" 
+        class= "genre-item"
+        :class="{ active: genre.id === genreStore.currentGenreId }">
+      {{ genre.name }}</li>
+    </ul>
     <loading v-model:active="isLoading" is-full-page />
 
   </div>
-    <div class="movie-list">
-      <div v-for="movie in movies" :key="movie.id" class="movie-card">
-    
-    <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
-    <div class="movie-details">
-      <p class="movie-title">{{ movie.title }}</p>
-      <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
-      <p class="movie-genres">
-        <span v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)">
-    {{ genreStore.getGenreName(genre_id) }}
-  </span></p>
+  <div class="movie-list">
+    <div v-for="movie in movies" :key="movie.id" class="movie-card">
+
+      <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+      <div class="movie-details">
+        <p class="movie-title">{{ movie.title }}</p>
+        <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
+        <p class="movie-genres">
+          <span  v-for="genre_id in movie.genre_ids" :key="genre_id" @click="listMovies(genre_id)"
+            :class="{ active: genre_id === genreStore.currentGenreId }" style="display: flex;justify-content: space-between;">
+            {{ genreStore.getGenreName(genre_id) }} 
+          </span>
+        </p>
+      </div>
     </div>
   </div>
-</div>
 
-<div class="footer"><h4>IfWachers</h4></div>
+  <div class="footer">
+    <h4>IfWachers</h4>
+  </div>
 </template>
 <style scoped>
-@import url('https://fonts.cdnfonts.com/css/cinzel');
-@import url('https://fonts.cdnfonts.com/css/qixohe-trial');
-
-
 .genre-list {
   font-family: 'Cinzel', sans-serif;
   display: flex;
@@ -80,8 +79,9 @@ const listMovies = async (genreId) => {
   list-style: none;
   margin-bottom: 2rem;
 
-  
+
 }
+
 .genre-item {
   margin-top: 20px;
   width: none;
@@ -90,7 +90,7 @@ const listMovies = async (genreId) => {
   align-items: center;
   background-color: #0000000e;
   border: 1px solid grey;
-  padding: 0.5rem ;
+  padding: 0.5rem;
   color: grey;
   transition: 0.5s;
 }
@@ -98,10 +98,11 @@ const listMovies = async (genreId) => {
 .genre-item:hover {
   cursor: pointer;
   background-color: #333;
-  color: #a03e32; 
+  color: #a03e32;
 
 
 }
+
 .movie-list {
   display: flex;
   flex-wrap: wrap;
@@ -110,13 +111,13 @@ const listMovies = async (genreId) => {
 }
 
 .movie-card {
-  font-family:  Arial, Helvetica, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   width: 15rem;
   height: 30rem;
   border-radius: 5px;
   overflow: hidden;
   border: 1px solid grey;
-  
+
 }
 
 .movie-card img {
@@ -160,7 +161,7 @@ const listMovies = async (genreId) => {
   background-color: #333;
 }
 
-.footer{
+.footer {
   position: fixed;
   font-family: 'Cinzel Decorative', sans-serif;
   bottom: 0;
@@ -170,7 +171,18 @@ const listMovies = async (genreId) => {
   align-items: center;
   justify-content: center;
   background-color: #333;
-  color: aliceblue  ;
+  color: aliceblue;
   padding: 1rem;
 }
+
+.active {
+  background-color: #333;
+  color: #a03e32;
+  font-weight: bolder;
+}
+
+.movie-genres span.active {
+  background-color: #333;
+  font-weight: bolder;
+  }
 </style>
