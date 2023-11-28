5232385013cc8470687339db82be77044b625f8b
eoFranca <erfranca.com@gmail.com>
2023-11-28 11:15:02 -0300

    atualização info movie

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -3,7 +3,13 @@ import { ref, onMounted } from 'vue'
 import api from '@/plugins/axios'
 import Loading from 'vue-loading-overlay'
 import genreStore from '@/stores/genre'
+    import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+function openMovie(movieId) {
+  router.push({ name: 'MovieDetails', params: { movieId } });
+}
 
 const isLoading = ref(false);
 
@@ -51,7 +57,11 @@ const listMovies = async (genreId) => {
   <div class="movie-list">
     <div v-for="movie in movies" :key="movie.id" class="movie-card">
 
-      <img :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" :alt="movie.title" />
+      <img
+  :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`"
+  :alt="movie.title"
+  @click="openMovie(movie.id)"
+/>
       <div class="movie-details">
         <p class="movie-title">{{ movie.title }}</p>
         <p class="movie-release-date">{{ formatDate(movie.release_date) }}</p>
