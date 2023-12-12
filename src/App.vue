212921656fd2d18d2d544546ef459207aa0ad167
eoFranca <erfranca.com@gmial.com>
2023-12-12 08:23:40 -0300

    navbar responsiva

--- src/App.vue
+++ src/App.vue
@@ -20,30 +20,31 @@ header {
   display: flex;
   background-color: #333; 
   color: #fff;
-  font-size: 1.2rem;
-  padding-left: 2rem;
+  font-size: 2vh;
+  padding-left: 2vh ;
   position: fixed;
   top: 0;
 }
 nav h1{
   color: #a03232;
   transition: 0.5s;
+  
 }
 
 nav h1:hover{
   color: #fff;
 }
 nav {
-  column-gap: 2rem;
+  column-gap: 2vh;
   margin-bottom: 0;
   display: flex;
   align-items: center;
 }
-
+  
 .navbar-link {
   text-decoration: none;
   color: #fff;
-  margin: 0 1rem; 
+  margin: 0 1vh; 
   transition: color 0.3s; 
 }
 
